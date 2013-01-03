99da0fcffe0f78705803df3f651802a6dd68abf8
tribalNerd <tribalNerd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-03 20:48:54 +0000

    Readme.txt Correction
    git-svn-id: https://plugins.svn.wordpress.org/wp-my-admin-bar/tags/0.2.0@647576 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/includes/my_tools.class.php
@@ -0,0 +1,171 @@
+<?php
+/**
+ * WP My Admin Bar
+ * @package WP My Admin Bar
+ * @author tribalNerd (tribalnerd@technerdia.com)
+ * @copyright Copyright (c) 2012, Chris Winters
+ * @link http://technerdia.com/projects/adminbar/plugin.html
+ * @license http://www.gnu.org/licenses/gpl.html
+ * @version 0.1.9
+ */
+
+
+
+/**
+ * ==================================== Creates a My Tools menu option in the Admin Toolbar
+ */
+
+if ( !defined( 'ABSPATH' ) ) { exit; } /* Wordpress check */
+
+class myTools {
+/**
+ * Create the Link
+ */
+	function myTools() {
+	/** Modified for version 0.1.7
+		if ( ! is_user_logged_in() )
+			return;
+
+		if ( !is_user_member_of_blog() && !is_super_admin() )
+			return;
+	*/
+			if ( !is_user_logged_in() && !is_admin_bar_showing() ) { return; }
+			if ( !current_user_can('manage_options') && !is_user_member_of_blog() ) { return; }
+
+		add_action( 'admin_bar_menu', array( $this, "menuTools" ),22 );
+	}
+
+/**
+ * Set the title of the menu item
+ */
+	function toolsTitle( $name, $id, $href = FALSE ) {
+		global $wp_admin_bar;
+		$wp_admin_bar->add_menu( array(
+			'id' 	=> $id,
+			'title' 	=> $name,
+			'href' 	=> $href )
+		);
+	}
+
+/**
+ * Create the sub menu option
+ */
+	function toolsOption( $name, $id, $root_menu, $href = FALSE ) {
+		global $wp_admin_bar;
+
+		$wp_admin_bar->add_group( array(
+			'parent' => $root_menu,
+			'id'     => 'my_tool_list',
+			'meta'   => array( 'class' => 'ab-sub-secondary' ) )
+		);
+
+		$wp_admin_bar->add_menu( array(
+			'title' 	=> $name,
+			'id' 	=> $id,
+			'parent' 	=> 'my_tool_list',
+			'href' 	=> $href )
+		);
+		
+	}
+
+/**
+ * Create Item Links
+ */
+	function externalItem( $id, $name, $link, $root_menu, $meta = TRUE ) {
+		global $wp_admin_bar;
+
+		$wp_admin_bar->add_menu( array(
+			'id' 		=> 'tools'. $id .'item',
+			'title' 	=> '&bull; '. __($name, 'wp-my-admin-bar') .' &raquo;',
+			'href' 	=> $link,
+			'parent' => $root_menu,
+			'meta' 	=> array( 'target' => '_blank' ) )
+		);
+	}
+
+/**
+ * Build The Menu
+ */
+	function menuTools() {
+		/** Modified for version 0.1.7
+			if ( !is_user_logged_in() ) { return; }
+			if ( !is_super_admin() || !is_admin_bar_showing() ) { return; }
+		*/
+			if ( !is_user_logged_in() && !is_admin_bar_showing() ) { return; }
+			if ( !current_user_can('manage_options') && !is_user_member_of_blog() ) { return; }
+
+		global $wp_admin_bar;
+
+		$tool_type_list = array(
+			"keywords" 	=> "Keyword Tools",
+			"seo" 		=> "SEO Tools",
+			"ranking" 	=> "Traffic &amp; Rankings",
+			"validate" 	=> "Validators",
+			"monitor" 	=> "Web Health",
+			"web" 		=> "Web Tools",
+			"words" 		=> "Word Tools"
+		);
+
+		$this->toolsTitle( "My Tools", "toolsTitle" );
+
+			foreach ( $tool_type_list as $key => $items ) {			
+				$this->toolsOption( $items, $key, "toolsTitle" );
+			}
+
+					$siteUrl = site_url();
+					$website = parse_url( $siteUrl );
+					$website = $website['host'];
+					$css_url = ''. get_bloginfo( 'template_url' ) .'/style.css';	/* used for css validator */
+					$feed_url = get_bloginfo( 'rss2_url' );					/* used to validated feed */
+				
+					/* Menu Text */
+					/* Keyword Tools */
+					$this->externalItem( "00", "Google KeyTool", "https://adwords.google.com/select/KeywordToolExternal", "keywords" );
+					$this->externalItem( "01", "Google Insights", "http://www.google.com/insights/search/", "keywords" );
+					$this->externalItem( "02", "Google Trends", "http://www.google.com/trends/", "keywords" );
+					$this->externalItem( "03", "Keyword Map", "http://www.kwmap.net/", "keywords" );
+					$this->externalItem( "04", "Keyword Spy", "http://www.keywordspy.com/research/search.aspx?q=$website&tab=domain-overview", "keywords" );
+					$this->externalItem( "05", "Meta Glossary", "http://www.metaglossary.com/", "keywords" );
+					$this->externalItem( "06", "SEO Digger", "http://analyticsdigger.org/search.php?q=$website", "keywords" );
+					$this->externalItem( "07", "Thesaurus", "http://thesaurus.com/", "keywords" );
+					$this->externalItem( "08", "Uber Suggest", "http://ubersuggest.org/", "keywords" );
+					$this->externalItem( "09", "Urban Dictionary", "http://www.urbandictionary.com/", "keywords" );
+					$this->externalItem( "10", "Wordtracker", "https://freekeywords.wordtracker.com/", "keywords" );
+					/* Traffic & Rankings */
+					$this->externalItem( "11", "Compete Rank", "http://siteanalytics.compete.com/$website/", "ranking" );
+					$this->externalItem( "12", "Alexa Rank", "http://www.alexa.com/siteinfo/$website", "ranking" );
+					/* SEO Tools */
+					$this->externalItem( "13", "Ahrefs Explorer", "https://ahrefs.com/site-explorer/backlinks/subdomains/$website", "seo" );
+					$this->externalItem( "14", "Majestic SEO", "http://www.majesticseo.com/reports/site-explorer/summary/$website?IndexDataSource=F", "seo" );
+					$this->externalItem( "15", "SEO Moz", "http://www.opensiteexplorer.org/links?site=$website", "seo" );
+					$this->externalItem( "16", "SEO Profiler", "http://www.seoprofiler.com/analyze/$website", "seo" );
+					$this->externalItem( "17", "SE Rush", "http://www.semrush.com/info/$website", "seo" );
+					/* Validators */
+					$this->externalItem( "18", "W3C Validate Page", "http://validator.w3.org/check?uri=$siteUrl/&charset=%28detect+automatically%29&doctype=Inline&group=0", "validate" );
+					$this->externalItem( "19", "W3C Validate CSS", "http://jigsaw.w3.org/css-validator/validator?uri=$css_url&profile=css21&usermedium=all&warning=1&vextwarning=&lang=en", "validate" );
+					$this->externalItem( "20", "W3C Mobile Checker", "http://validator.w3.org/mobile/check?docAddr=$website&async=true", "validate" );
+					$this->externalItem( "21", "W3C Validate Feed", "http://validator.w3.org/feed/check.cgi?url=$feed_url", "validate" );
+					$this->externalItem( "22", "W3C Link Checker", "http://validator.w3.org/checklink?uri=$website&hide_type=all&depth=&check=Check", "validate" );
+					/* Web Health */
+					$this->externalItem( "23", "Internet Conditions", "http://www.akamai.com/html/technology/dataviz1.html", "monitor" );
+					$this->externalItem( "24", "Internet Health", "http://www.internetpulse.net/", "monitor" );
+					$this->externalItem( "25", "Internet Health Map", "http://www.gomez.com/internet-health-map/", "monitor" );
+					$this->externalItem( "26", "Traffic Report", "http://www.internettrafficreport.com/", "monitor" );
+					$this->externalItem( "27", "Google Servers", "http://www.google.com/transparencyreport/traffic/", "monitor" );
+					/* Web Tools */
+					$this->externalItem( "28", "Domain Whois", "http://whois.domaintools.com/", "web" );
+					$this->externalItem( "29", "Down For Me?", "http://www.downforeveryoneorjustme.com/", "web" );
+					$this->externalItem( "30", "iWeb Tools", "http://www.iwebtool.com/tools/", "web" );
+					$this->externalItem( "31", "Pingler Ping Tool", "http://pingler.com/", "web" );
+					$this->externalItem( "32", "Pingler Web Tools", "http://pingler.com/seo-tools/", "web" );
+					$this->externalItem( "33", "Qbase Site Data", "http://www.quarkbase.com/$website", "web" );
+					$this->externalItem( "34", "WayBackMachine", "http://www.archive.org/", "web" );
+					/* Word Tools */
+					$this->externalItem( "35", "Combine Words", "http://www.combinewords.com/", "words" );
+					$this->externalItem( "36", "Keyword Density", "http://tools.davidnaylor.co.uk/keyworddensity/", "words" );
+					$this->externalItem( "37", "Keyword Mixer", "http://keywordmixer.com/", "words" );
+					$this->externalItem( "38", "Readability Index", "http://www.standards-schmandards.com/exhibits/rix/index.php", "words" );
+					$this->externalItem( "39", "Typo Generator", "http://tools.seobook.com/spelling/keywords-typos.cgi", "words" );
+	} /* end function */
+} /* end class */
+?>
---
+++ trunk/includes/settings_network.class.php
@@ -0,0 +1,250 @@
+<?php
+/**
+ * WP My Admin Bar
+ * @package WP My Admin Bar
+ * @author tribalNerd (tribalnerd@technerdia.com)
+ * @copyright Copyright (c) 2012, Chris Winters
+ * @link http://technerdia.com/projects/adminbar/plugin.html
+ * @license http://www.gnu.org/licenses/gpl.html
+ * @version 0.1.9
+ */
+
+
+
+/**
+ * ==================================== Settings Page for Multisite Network Admin
+ */
+
+if ( !defined( 'ABSPATH' ) ) { exit; } /* Wordpress check */
+
+class MyAdminBar_Network_Admin {
+/**
+ * Network Settings Page
+ */
+	function my_siteadmin_submenu() {
+		add_submenu_page( 'settings.php', 'Admin Bar', 'Admin Bar', 'manage_options', 'my_admin_bar.php', array( &$this, 'wp_mynetworkadmin' ) ); 	/* calls core function wp_mynetworkadmin() */
+	}
+
+/**
+ * Set Defaults
+ */
+	function __construct() {
+		add_action( 'network_admin_menu', array( &$this, 'my_siteadmin_submenu' ) ); /* calls submenu  my_siteadmin_submenu() */
+	} /* end function __construct() */
+	
+
+/**
+ * Core Function: Update Options on Post and Build Network Template
+ */
+	function wp_mynetworkadmin() { /* called from wp_myadminbar_submenu() */
+/**
+ * Build wp_myadminbar Option based on posted information
+ */
+		if ( isset( $_POST['save_my_menus'] ) && check_admin_referer( 'my_option_action', 'my_option_nonce' ) ) {
+			if ( $_POST['my_sites'] == "show" || $_POST['my_sites'] == "hide" ) { $my_sites_post = $_POST['my_sites']; }
+			if ( $_POST['my_cache'] == "show" || $_POST['my_cache'] == "hide" ) { $my_cache_post = $_POST['my_cache']; }
+			if ( $_POST['my_tools'] == "show" || $_POST['my_tools'] == "hide" ) { $my_tools_post = $_POST['my_tools']; }
+	
+			$options_array = array(
+				'my_sites' 	=> $my_sites_post,
+				'my_cache' 	=> $my_cache_post,
+				'my_tools' 	=> $my_tools_post
+			);
+
+			if ( get_option('wp_myadminbar') || get_option('wp_myadminbar_nw') ) { /* Keep It Clean */
+				delete_option( 'wp_myadminbar' );
+				delete_option( 'wp_myadminbar_nw' );
+			}
+
+			$this->wp_myadminbar_blogs( $the_option = 'wp_myadminbar', $options_array ); 	/* Rebuild Option */
+			
+			/* Add A Network Only Option */
+			switch_to_blog(1);
+			add_option( 'wp_myadminbar_nw', serialize( $options_array ), '', 'no' );
+		}
+
+/**
+ * Build wp_mycache Option based on posted information
+ */
+		if ( isset( $_POST['save_my_cache'] ) && check_admin_referer( 'my_cache_action', 'my_cache_nonce' ) ) {
+			if ( $_POST['dbcache'] == "show" || $_POST['dbcache'] == "hide" ) { $dbcache_post = $_POST['dbcache']; }
+			if ( $_POST['widget'] == "show" || $_POST['widget'] == "hide" ) { $widget_post = $_POST['widget']; }
+			if ( $_POST['minify'] == "show" || $_POST['minify'] == "hide" ) { $minify_post = $_POST['minify']; }
+			if ( $_POST['super'] == "show" || $_POST['super'] == "hide" ) { $super_post = $_POST['super']; }
+			if ( $_POST['total'] == "show" || $_POST['total'] == "hide" ) { $total_post = $_POST['total']; }
+	
+			$options_array = array(
+				'dbcache' 	=> $dbcache_post,
+				'widget' 	=> $widget_post,
+				'minify' 	=> $minify_post,
+				'super' 		=> $super_post,
+				'total' 		=> $total_post
+			);
+
+			if ( get_option( 'wp_mycache' ) || get_option( 'wp_mycache_nw' ) ) { /* Keep It Clean */
+				delete_option( 'wp_mycache' );
+				delete_option( 'wp_mycache_nw' );
+			}
+
+			$this->wp_myadminbar_blogs( $the_option = 'wp_mycache', $options_array ); /* Rebuild Option */
+
+			/* Add A Network Only Option */
+			switch_to_blog(1);
+			add_option( 'wp_mycache_nw', serialize( $options_array ), '', 'no' );
+		}
+
+/**
+ * Build wp_mycustom Option based on posted information
+ */
+		if ( isset( $_POST['save_my_settings'] ) && check_admin_referer( 'my_custom_action', 'my_custom_nonce' ) ) {
+			if ( $_POST['wplogo'] == "show" || $_POST['wplogo'] == "hide" ) { $wplogo_post = $_POST['wplogo']; }
+			if ( $_POST['howdy'] == "show" || $_POST['howdy'] == "hide" ) { $howdy_post = $_POST['howdy']; }
+			if ( $_POST['wpicon'] == "show" || $_POST['wpicon'] == "hide" ) { $wpicon_post = $_POST['wpicon']; }
+			if ( $_POST['siteids'] == "show" || $_POST['siteids'] == "hide" ) { $siteids_post = $_POST['siteids']; }
+
+			$options_array = array(
+				'wplogo' 	=> $wplogo_post,
+				'howdy' 		=> $howdy_post,
+				'wpicon' 	=> $wpicon_post,
+				'siteids' 	=> $siteids_post
+			);
+
+			if ( get_option( 'wp_mycustom' ) ) { /* Keep It Clean */
+				delete_option( 'wp_mycustom' );
+			}
+
+			$this->wp_myadminbar_blogs( $the_option = 'wp_mycustom', $options_array ); /* Rebuild Option */
+		}
+
+
+	/* display template */
+		$this->show_template();
+	} /* end wp_mynetworkadmin() */
+
+
+/**
+ * Run setting changes through each Website within the Network.
+ */
+	function wp_myadminbar_blogs( $the_option, $options_array ){
+		global $wpdb;
+
+			/**
+			 * No Transient Check, Delete Transient, Get DB Query, Reset Transient.
+			 * Helps make sure transient cache is correct, resets expire.
+			 */
+				delete_site_transient( 'multisite_site_list' );
+
+				$network_blog_id = $wpdb->get_results( $wpdb->prepare( "SELECT blog_id FROM $wpdb->blogs WHERE site_id = %d AND public = '1' AND archived = '0' AND spam = '0' AND deleted = '0' ORDER BY blog_id", $wpdb->siteid ) );
+				
+				set_site_transient( 'multisite_site_list', $network_blog_id, 86400 );
+
+		foreach ( $network_blog_id as $current_blog_id ) {
+			switch_to_blog( $current_blog_id->blog_id );
+
+			if ( get_option( $the_option ) ) { /* Keep It Clean */
+				delete_option( $the_option );
+			}
+
+			add_option( $the_option, serialize( $options_array ), '', 'no' );
+		}
+
+		restore_current_blog();
+			print '<div class="updated"><p><strong>'. __('Network Wide Settings Saved. If the admin bar did not visually change, you may need to', 'wp-my-admin-bar') .' <a href="http://'.$_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'].'">'. __('refresh this page', 'wp-my-admin-bar') .'</a>.</strong></p></div>';
+		return;
+	} /* end wp_myadminbar_blogs() */
+
+
+/**
+ * Network Settings Page Template
+ */
+	function show_template() {
+		$checked = 'checked="checked"';
+
+		/* MyFunctions Class */
+			$my_functions_class = new MyFunctions();
+		/** Adjusted for Version 0.1.7
+			$my_menus = $my_functions_class->option_wp_myadminbar();
+			$my_cache = $my_functions_class->option_wp_mycache();
+		*/
+			$my_menus = $my_functions_class->option_wp_myadminbar_nw();				/* get option values */		
+			$my_cache = $my_functions_class->option_wp_mycache_nw();					/* get option values */
+			$my_custom = $my_functions_class->option_wp_mycustom();					/* get option values */
+
+		/** Adjusted for Version 0.1.7
+			$myadminbar_option = $my_functions_class->wp_myadminbar_status();
+			$mycache_option = $my_functions_class->wp_mycache_status();
+		*/
+			$myadminbar_option = $my_functions_class->wp_myadminbar_nw_status();	/* check option */			
+			$mycache_option = $my_functions_class->wp_mycache_nw_status();			/* check option */
+			$mycustom_option = $my_functions_class->wp_mycustom_status();			/* check option */
+
+/**
+ * Checked Items for Menu Options
+ */
+			if ( $my_menus['my_sites'] == "show" ) { $sites_on = $checked; }else{ $sites_off = $checked; }
+			if ( $my_menus['my_cache'] == "show" ) { $cache_on = $checked; }else{ $cache_off = $checked; }
+			if ( $my_menus['my_tools'] == "show" ) { $tools_on = $checked; }else{ $tools_off = $checked; }
+			if ( !$myadminbar_option ) { $myadminbar_option = __('The WP-MyAdminBar Option Value is Blank.', 'wp-my-admin-bar'); }
+
+/**
+ * Checked Items for Cache Plugins
+ */
+			if ( $my_cache['dbcache'] == "show" ) { $dbcache_on = $checked; }else{ $dbcache_off = $checked; }
+			if ( $my_cache['widget'] == "show" ) { $widget_on = $checked; }else{ $widget_off = $checked; }
+			if ( $my_cache['minify'] == "show" ) { $minify_on = $checked; }else{ $minify_off = $checked; }
+			if ( $my_cache['super'] == "show" ) { $super_on = $checked; }else{ $super_off = $checked; }
+			if ( $my_cache['total'] == "show" ) { $total_on = $checked; }else{ $total_off = $checked; }
+			if ( !$mycache_option ) { $mycache_option = __('The My Cache Menu Option Value is Blank.', 'wp-my-admin-bar'); }
+
+/**
+ * Checked Items for Custom Settings
+ */
+		if ( $my_custom['wplogo'] == "show" ) {
+			$wplogo_on = $checked;
+		}elseif( $my_custom['wplogo'] == "hide" ){
+			$wplogo_off = $checked;
+		}else{
+			$wplogo_on = $checked;
+		}
+
+		if ( $my_custom['howdy'] == "show" ) {
+			$howdy_on = $checked;
+		}elseif( $my_custom['howdy'] == "hide" ){
+			$howdy_off = $checked;
+		}else{
+			$howdy_on = $checked;
+		}
+
+		if ( $my_custom['wpicon'] == "show" ) {
+			$wpicon_on = $checked;
+		}elseif( $my_custom['wpicon'] == "hide" ){
+			$wpicon_off = $checked;
+		}else{
+			$wpicon_on = $checked;
+		}
+
+		if ( $my_custom['siteids'] == "show" ) {
+			$siteids_on = $checked;
+		}elseif( $my_custom['siteids'] == "hide" ){
+			$siteids_off = $checked;
+		}else{
+			$siteids_off = $checked;
+		}
+			
+		if ( !$mycustom_option ) { $mycustom_option = __('The Custom Settings Option Value is Blank.', 'wp-my-admin-bar'); }
+
+/**
+ * Tabs
+ */
+	$display_tabs = new displayTabs();
+	$setting_tabs = $display_tabs->settings_tabs();
+
+/**
+ * The Template
+ */
+		ob_start();
+			include MYAB_TEMPLATES . '/settings_network.php';
+		ob_end_flush();
+	} /* show_template() */	
+} /* end class WP_MyAdminBar_Network */
+?>
